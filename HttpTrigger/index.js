9cbe622a2e7b9ebd9b67ba013e752d79731dfbfe
MukundhBhushan <aknsmb@gmail.com>
2019-06-01 01:17:11 +0530

    adding files

---
+++ HttpTrigger/index.js
@@ -0,0 +1,20 @@
+var http = require('http');
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+    var options = {
+        path: '/test',
+        method: 'POST'
+    };
+    if (req.query.name || (req.body && req.body.name)) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: "Hello " + (req.query.name || req.body.name)  
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+};  
